4ad2c3a3a920e3fd9a9e963e2b967655fd0f8010
Karl Coelho <karl.coelho1@gmail.com>
2019-10-30 15:21:10 -0400

    Add some content

--- src/App.js
+++ src/App.js
@@ -5,20 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Karl Coelho</h1>
+      <p>I'm a software engineer and student.</p>
+
+      <div className="links">
+        <a href="https://github.com/karlcoelho">Github</a> | &nbsp;
+        <a href="https://linkedin.com/in/karlcoelho">LinkedIn</a> | &nbsp;
+        <a href="mailto:karl@gmail.com">Email</a>
+      </div>
     </div>
   );
 }
